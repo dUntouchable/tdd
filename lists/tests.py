336656ca55148838f1da21a0d51b677e209b4db6
debasis <debasis.untouchable@gmail.com>
2019-10-17 15:36:02 +0530

    filled html ids, frontpage is generated now

--- lists/tests.py
+++ lists/tests.py
@@ -12,10 +12,5 @@ class HomepageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.strip().startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
+        
         self.assertTemplateUsed(response, 'home.html')
