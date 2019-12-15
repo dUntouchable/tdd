c8f1171907ac5e80153470264d26bb332fecb525
debasis <debasis.untouchable@gmail.com>
2019-12-15 14:30:10 +0530

    Model Item created, added text and inheritated from models.Model, added Unit Test Cases

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.models import Item
 
 # Create your tests here.
 class HomepageTest(TestCase):
@@ -12,5 +13,25 @@ class HomepageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-        
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', {'item_text' : 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'First Item'
+        first_item.save()
+
+        sec_item = Item()
+        sec_item.text = 'Second Item'
+        sec_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        self.assertEqual(saved_items[0].text, 'First Item')
+        self.assertEqual(saved_items[1].text, 'Second Item')
